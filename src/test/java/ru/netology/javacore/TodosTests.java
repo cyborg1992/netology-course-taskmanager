6d1f5f7402f09860133be3aaed33b490aef13fc3
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-07-02 01:13:13 +0300

    add tests for class Todos

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,40 @@
 package ru.netology.javacore;
 
-public class TodosTests {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    // ваши тесты для класса Todos
+public class TodosTests {
+    Todos todos;
+
+    @BeforeEach
+    void setUp() {
+        todos = new Todos();
+        todos.addTask("test");
+    }
+
+    @AfterEach
+    void tearDown() {
+        todos = null;
+    }
+
+    @Test
+    void testAddTask() {
+        Assertions.assertTrue(todos.addTask("test2"));
+        Assertions.assertFalse(todos.addTask("test"));
+    }
+
+    @Test
+    void testRemoveTask() {
+        Assertions.assertTrue(todos.removeTask("test"));
+        Assertions.assertFalse(todos.removeTask("test"));
+    }
+
+    @Test
+    void testGetAllTasks() {
+        String expected = "test";
+        String actual = todos.getAllTasks();
+        Assertions.assertEquals(expected, actual);
+    }
 }
